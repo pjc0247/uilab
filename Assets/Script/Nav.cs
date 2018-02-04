cfd30aea35dca7e49837fe481de7643c617d363d
Park June Chul <pjc0247@naver.com>
2018-02-04 21:48:17 +0900

    asd

--- Assets/Script/Nav.cs
+++ Assets/Script/Nav.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class Nav : MonoBehaviour
 {
+    public enum Scene
+    {
+        AppDetail
+    }
+
     public static Nav instance;
+
+    public RectTransform appDetail;
 
     void Awake()
     {
@@ -14,4 +21,27 @@ public class Nav : MonoBehaviour
     {
 		
 	}
+
+    public void OnClickGameProjects()
+    {
+        Push(Scene.AppDetail);
+    }
+
+    public void Push(Scene scene)
+    {
+        StartCoroutine(SlideToFunc(0, appDetail));
+    }
+    public void Pop()
+    {
+        StartCoroutine(SlideToFunc(1218, appDetail));
+    }
+    IEnumerator SlideToFunc(int x, RectTransform t)
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            t.anchoredPosition += ( 
+                new Vector2(x, t.anchoredPosition.y) - t.anchoredPosition) * 0.35f;
+            yield return null;
+        }
+    }
 }
