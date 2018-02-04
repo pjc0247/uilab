db69a50efd03b3bf61fd7cb64c556fb59afe1fb7
Park June Chul <pjc0247@naver.com>
2018-02-05 02:30:08 +0900

    dd

---
+++ Assets/Script/ProfilePage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProfilePage : MonoBehaviour
+{
+    public RectTransform devSpecs;
+    public ScrollRect scroll;
+
+    public RectTransform profileImage;
+
+    private Vector2 originalProfileImagePosition;
+
+    void Start () {
+        originalProfileImagePosition = profileImage.anchoredPosition;
+
+    }
+
+    public void OnScrollValueChanged(Vector2 v)
+    {
+        var y = scroll.content.sizeDelta.y - scroll.content.sizeDelta.y * v.y;
+
+        Debug.Log(y);
+        if (y <= 2500)
+        {
+            profileImage.anchoredPosition = originalProfileImagePosition - new Vector2(0, y/4);
+        }
+        devSpecs.localScale = Vector2.one * (Mathf.Clamp(y / 2500, 0, 1));
+    }
+}
