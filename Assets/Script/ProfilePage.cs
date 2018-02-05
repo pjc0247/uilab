0eb545ec5330c7b708a7941d9d8b9dc1d83bc10c
Park June Chul <pjc0247@naver.com>
2018-02-05 19:46:53 +0900

    zuzu

--- Assets/Script/ProfilePage.cs
+++ Assets/Script/ProfilePage.cs
@@ -11,21 +11,66 @@ public class ProfilePage : MonoBehaviour
     public RectTransform profileImage;
 
     private Vector2 originalProfileImagePosition;
+    private Dictionary<RectTransform, int> devSpecsTiming;
+    private Dictionary<RectTransform, Vector2> devSpecsOriginalPositions;
 
-    void Start () {
+    void Start ()
+    {
         originalProfileImagePosition = profileImage.anchoredPosition;
 
+        devSpecsTiming = new Dictionary<RectTransform, int>();
+        devSpecsOriginalPositions = new Dictionary<RectTransform, Vector2>();
+        for (int i = 0; i < devSpecs.childCount; i++)
+        {
+            var c = devSpecs.GetChild(i).GetComponent<RectTransform>();
+            devSpecsOriginalPositions[c] = c.anchoredPosition;
+            devSpecsTiming[c] = Random.Range(0, 2000);
+            c.anchoredPosition = Vector2.zero;
+            c.localScale = Vector3.zero;
+        }
     }
 
     public void OnScrollValueChanged(Vector2 v)
     {
-        var y = scroll.content.sizeDelta.y - scroll.content.sizeDelta.y * v.y;
+        var y = scroll.content.anchoredPosition.y;
 
         Debug.Log(y);
-        if (y <= 2500)
+        if (y <= 3350)
         {
-            profileImage.anchoredPosition = originalProfileImagePosition - new Vector2(0, y/4);
+            profileImage.anchoredPosition = originalProfileImagePosition - new Vector2(0, y/(1.0f));
         }
-        devSpecs.localScale = Vector2.one * (Mathf.Clamp(y / 2500, 0, 1));
+        foreach (var p in devSpecsTiming)
+        {
+            if (y <= p.Value)
+            {
+                p.Key.anchoredPosition = Vector2.zero;
+                p.Key.localScale = Vector2.zero;
+            }
+            else
+            {
+                p.Key.anchoredPosition = devSpecsOriginalPositions[p.Key] * BounceOut(y / (3300 - p.Value));
+                p.Key.localScale = Vector2.one * BounceOut(y / (3300 - p.Value));
+            }
+        }
+    }
+
+    float BounceOut(float t)
+    {
+        t = Mathf.Clamp(t, 0, 1);
+
+        if (t < (1.0f / 2.75f))
+            return 7.5625f * t * t;
+        if (t < (2.0f / 2.75f))
+        {
+            t -= 1.5f / 2.75f;
+            return 7.5625f * t * t + 0.75f;
+        }
+        if (t < (2.5f / 2.75f))
+        {
+            t -= 2.25f / 2.75f;
+            return 7.5625f * t * t + 0.9375f;
+        }
+        t -= 2.625f / 2.75f;
+        return 7.5625f * t * t + 0.984375f;
     }
 }
