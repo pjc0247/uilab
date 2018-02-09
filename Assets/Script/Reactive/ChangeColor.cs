5bacb19b29a1058d3e07035d16d0480e2bc209a9
Park June Chul <pjc0247@naver.com>
2018-02-10 01:54:38 +0900

    update

---
+++ Assets/Script/Reactive/ChangeColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColor : ScrollReactive
+{
+    public int targetY;
+    public Color targetColor;
+    public Image img;
+
+    public void OnScroll(int y)
+    {
+        if (y <= targetY - 1000) return;
+
+        var blend = Mathf.Clamp((float)(targetY - y) / 500, 0, 1);
+        img.color = Color.white * blend + (targetColor * (1 - blend));
+        img.color += new Color(0, 0, 0, 1);
+    }
+}
---
+++ Assets/Script/Reactive/KeepPosition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepPosition : ScrollReactive
+{
+    public int targetY;
+
+    private RectTransform rt;
+    private Vector2 originalPosition;
+
+    void Awake()
+    {
+        rt = GetComponent<RectTransform>();
+        originalPosition = rt.anchoredPosition;
+    }
+
+	public void OnScroll(int y)
+    {
+        if (y <= targetY - 400) return;
+
+        rt.anchoredPosition = originalPosition - new Vector2(0, y - targetY);
+    }
+}
