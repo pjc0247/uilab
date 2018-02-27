298b7bfd0ca0c745470ca409cb490c7dd05996f7
Park June Chul <pjc0247@naver.com>
2018-02-27 23:01:05 +0900

    asdfsa

---
+++ Assets/Script/Lockscreen/Lockscreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class Lockscreen : MonoBehaviour
+{
+    public Text clock;
+    public Text date;
+
+    void Update()
+    {
+        var now = DateTime.Now;
+        clock.text = now.Hour + ":" + (now.Minute < 10 ? "0" + now.Minute : now.Minute.ToString());
+        date.text = now.DayOfWeek + ", " + now.ToString("MMMM", CultureInfo.InvariantCulture) + " " + now.Day;
+    }
+}
---
+++ Assets/Script/Lockscreen/SwipeToUnlock.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SwipeToUnlock : UiBase,
+    IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    private RectTransform parentRt;
+
+    void Start()
+    {
+        parentRt = transform.parent.GetComponent<RectTransform>();
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        var rootRt = GetComponentInParent<CanvasScaler>().GetComponent<RectTransform>();
+        var y = 2637.0f / Screen.height * Input.mousePosition.y;
+
+        parentRt.anchoredPosition = new Vector2(0, y);
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (parentRt.anchoredPosition.y >= 1024)
+            MoveTo(parentRt, 8, new Vector2(0, 3000), Easing.SineOut);
+        else
+            MoveTo(parentRt, 13, new Vector2(0, 0), Easing.SineOut);
+    }
+}
